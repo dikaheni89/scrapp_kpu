52615244019d10821a916112bccdaec270f8ae2a
Handika Sutajaba <digitalsoftware957@gmail.com>
2024-02-05 11:57:37 +0700

    update code project

---
+++ README.md
@@ -0,0 +1,33 @@
+
+## Overview
+
+This is a starter template using the following stack:
+
+- TechStack - [Node.js 20](https://nextjs.org/13)
+
+## Pages
+
+| Pages                                                                             | Specifications                                                                                        |
+| :-------------------------------------------------------------------------------- | :---------------------------------------------------------------------------------------------------- |
+| [Signup](https://next-shadcn-dashboard-starter.vercel.app/)                       | Authentication with **NextAuth** supports Social logins and email logins(Enter dummy email for demo). |
+| [Dashboard](https://next-shadcn-dashboard-starter.vercel.app/dashboard)           | Cards with recharts graphs for analytics.                                                             |
+| [Users](https://next-shadcn-dashboard-starter.vercel.app/dashboard/user)          | Tanstack tables with user details client side searching, pagination etc                               |
+| [Users/new](https://next-shadcn-dashboard-starter.vercel.app/dashboard/user/new)  | A User Form with Uploadthing to support file uploading with dropzone.                                 |
+| [Employee](https://next-shadcn-dashboard-starter.vercel.app/dashboard/employee)   | Tanstack tables with server side searching, pagination etc).                                          |
+| [Profile](https://next-shadcn-dashboard-starter.vercel.app/dashboard/profile)     | Mutistep dynamic forms using react-hook-form and zod for form validation.                             |
+| [Kanban Board](https://next-shadcn-dashboard-starter.vercel.app/dashboard/kanban) | A Drag n Drop task management board with dnd-kit and zustand to persist state locally.                |
+| [Not Found](https://next-shadcn-dashboard-starter.vercel.app/dashboard/notfound)  | Not Found Page Added in the root level                                                                |
+| -                                                                                 | -                                                                                                     |
+
+## Getting Started
+
+Clone this repo
+
+Run the following commands to start the development server:
+
+```
+npm install
+nodemon .
+```
+
+You should now be able to access the application at http://localhost:3000.
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const express = require('express');
 const cheerio = require('cheerio');
 const readline = require('readline');
 const process = require("process");
+const { url } = require('inspector');
 const app = express()
 const port = 5000;
 
@@ -16,62 +17,84 @@ const connectPg = new Client({
     user: 'postgres',
     host: 'localhost',
     database: 'scrape_kpu',
-    password: '12345678',
+    password: 'postgres',
     port: 5432,
 })
 
 // Wrap our question within a new function
 const question = () => {
-  return new Promise((resolve) => {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
+    return new Promise((resolve) => {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
 
-    rl.question('Masukan Url Srappernya Selanjutnya: ', (url) => {
-      rl.close();
-      resolve(url);
+        rl.question('Masukan Url Srappernya Selanjutnya: ', (url) => {
+            rl.close();
+            resolve(url);
+        });
     });
-  });
 };
 
 
 const getPlayDataCaleg = async () => {
+
     const url = await question(); // Get user's input using the new function
     let result = await clients.get(url);
+    // console.log(result);
     const regex = /<[^>]*>/g; // This regex matches all HTML tags
     const html = result.data['data'].toString();
+    // console.log(html);
     let dataReplace = html.replace(regex, '');
-    const dataConvert = dataReplace.split(',').map(item => item.trim());
+    const dataConvert = dataReplace.split(",").map(item => item.trim());
+    // console.log('dataConvert', dataConvert);
 
     const candidates = [];
     for (let i = 0; i < dataConvert.length; i++) {
         if (dataConvert[i].startsWith('Partai') && dataConvert[i + 2].startsWith('nomor urut')) {
-            const candidate = {
-                party: dataConvert[i],
-                district: dataConvert[i + 1],
-                number: dataConvert[i + 2],
-                name: dataConvert[i + 4],
-                gender: dataConvert[i + 5],
-                gelar: dataConvert[i + 6],
-                city: dataConvert[i + 7],
-            };
+            const candidate = [
+                dataConvert[i],
+                dataConvert[i + 1],
+                dataConvert[i + 2],
+                dataConvert[i + 4],
+                dataConvert[i + 5],
+                dataConvert[i + 6],
+                dataConvert[i + 7],
+                dataConvert[i + 8],
+            ];
             candidates.push(candidate);
-
         }
     }
 
-    for (let i = 0; i < candidates.length; i++) {
-        await connectPg.query("INSERT INTO caleg (party, district, number, name, gender, gelar, city) VALUES ($1, $2, $3, $4, $5, $6, $7)", [
-            candidates[i].party,
-            candidates[i].district,
-            candidates[i].number,
-            candidates[i].name,
-            candidates[i].gender,
-            candidates[i].gelar,
-            candidates[i].city
+    const MAX_GELAR = 3
+    let sanitizedGelar = candidates.map(item => {
+        // index jenis kelamin
+        const indexJenisKelamin = item.findIndex(item2 => item2 === 'LAKI - LAKI' || item2 === 'PEREMPUAN')
+
+        return {
+            partai: item[0],
+            dapil: item[1].replace('Dapil', ''),
+            number: item[2].replace('nomor urut', ''),
+            name: item[3],
+            gelar: item.slice(4, indexJenisKelamin).join(', '),
+            gender: item[indexJenisKelamin],
+            city: item[indexJenisKelamin + 1]
+        }
+    })
+
+    // console.log('sanitized', sanitizedGelar);
+
+    for (let i = 0; i < sanitizedGelar.length; i++) {
+        await connectPg.query("INSERT INTO caleg (partai, dapil, number, name, gender, city, level_dapil) VALUES ($1, $2, $3, $4, $5, $6, $7)", [
+            sanitizedGelar[i].partai,
+            sanitizedGelar[i].dapil,
+            'No Urut ' + sanitizedGelar[i].number,
+            sanitizedGelar[i].name +' '+ sanitizedGelar[i].gelar,
+            sanitizedGelar[i].gender,
+            sanitizedGelar[i].city,
+            'DPRD PROVINSI'
         ]);
-        console.log(`Progress: Inserted ${i+1} of ${candidates.length}`);
+        console.log(`Progress: Inserted ${i+1} of ${sanitizedGelar.length}`);
     }
     console.log('Done');
     // process.exit(); // Don't exit the process. Instead, ask the user for new input
@@ -87,5 +110,5 @@ connectPg.connect(async function (err) {
 });
 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
-    console.log(`Starting Scrapping in ${port}`); 
+    console.log(`Starting Scrapping in ${port}`);
 });
